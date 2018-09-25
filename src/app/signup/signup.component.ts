f0c722c52cd84ce1a330df7158687fdbf2eaf6bf
sabinbogati <sabin.bogati@convergetree.com>
2018-09-25 11:31:07 +0545

    Validation Working Now...

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -15,14 +15,14 @@ export class SignupComponent implements OnInit {
   ngOnInit() {
 
     this.signupForm = new FormGroup({
-      "firstName": new FormControl(null),
-      "lastName": new FormControl(null),
-      "address": new FormControl(null),
-      "phoneNumber": new FormControl(null),
-      "email": new FormControl(null),
-      "conformEmail": new FormControl(null),
-      "password": new FormControl(null),
-      "conformPassword": new FormControl(null)
+      "firstName": new FormControl(null,Validators.required),
+      "lastName": new FormControl(null, Validators.required),
+      "address": new FormControl(null, Validators.required),
+      "phoneNumber": new FormControl(null, Validators.required),
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "conformEmail": new FormControl(null, [Validators.required, Validators.email]),
+      "password": new FormControl(null, Validators.required),
+      "conformPassword": new FormControl(null, Validators.required)
 
     })
   }
